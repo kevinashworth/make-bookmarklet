a804fa68da2790fcefdc89d852cd78e1096bd06f
Kevin Ashworth <kevinashworth@yahoo.com>
2020-08-01 18:38:18 -0700

    Improve regex

--- make-bookmarklet.js
+++ make-bookmarklet.js
@@ -36,16 +36,22 @@ if (source) {
     .replace(/[ ]{2,}/g, ' ') // Space runs to one space
     .replace(/^\s+/gm, '') // Remove line-leading whitespace
     .replace(/\s+$/gm, '') // Remove line-ending whitespace
-    .replace(/\s+(\+|-|\*|\/|\*\*|%|\+\+|--)\s+/g, '$1') // Remove whitespace before, after operators
-    .replace(/\s+(>|<|<=|>=|!=|!==|==|===)\s+/g, '$1') // Remove whitespace before, after comparators
-    .replace(/\s+(=|\+=|-=|\*=|\/=|%=)\s+/g, '$1') // Remove whitespace before, after assignment
-    .replace(/\s+(\(|{|\[|\)|}|\])\s+/g, '$1') // Remove whitespace before, after parens/braces/brackets
-    .replace(/\s+,\s+/g, ','); // Remove whitespace before, after ','
+    .replace(/\s?(\+|-|\*|\/|\*\*|%|\+\+|--|&&|\|\|)\s?/g, '$1') // Remove whitespace before, after operators
+    .replace(/\s?(>|<|<=|>=|!=|!==|==|===)\s?/g, '$1') // Remove whitespace before, after comparators
+    .replace(/\s?(=|\+=|-=|\*=|\/=|%=)\s?/g, '$1') // Remove whitespace before, after assignment
+    .replace(/\s?(\(|{|\[|\)|}|\])\s?/g, '$1') // Remove whitespace before, after parens/braces/brackets
+    .replace(/\s?(,|;)\s?/g, '$1'); // Remove whitespace before, after punctuation
   if (program.aggressive) {
     bookmarklet = bookmarklet
       .replace(/(const|let|var)\s+/g, '') // Remove variable declarations
       .replace(/window.(\w)/g, '$1') // Remove window object
       .replace(/===/, '=='); // Use equality instead of identity comparison
+  }
+  if (program.debug) {
+    console.log(' ');
+    console.log('// [debug] bookmarklet before encoding');
+    console.log(bookmarklet);
+    console.log(' ');
   }
   const encode = program.component ? encodeURIComponent : encodeURI;
   bookmarklet = 'javascript:' + encode(bookmarklet); // Escape semicolons, double quotes, etc.
