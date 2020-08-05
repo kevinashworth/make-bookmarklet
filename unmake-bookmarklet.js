232142b9bd1db8a51b09aec3b3119350b1a961f7
Kevin Ashworth <kevinashworth@yahoo.com>
2020-08-04 20:45:41 -0700

    Finish 1.1.1 - same for unmake

--- unmake-bookmarklet.js
+++ unmake-bookmarklet.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 const fs = require('fs');
 const { program } = require('commander');
+var chalk = require('chalk');
+var error = chalk.bold.red;
+var success = chalk.bold.green;
+var verbose = chalk.bold.yellow;
 
 let filename;
 program
@@ -9,12 +13,18 @@ program
   .action((results) => {
     filename = results;
   });
+program.option('-d, --debug', 'verbose output to the command line');
 program.parse(process.argv);
 
 let bookmarklet;
 const source = fs.readFileSync(filename, 'utf8');
 
 if (source) {
+  if (program.debug) {
+    console.log(verbose('// [debug] input'));
+    console.log(source);
+    console.log(' ');
+  }
   bookmarklet = source.replace(/^\s?javascript:/gm, '');
   bookmarklet = decodeURIComponent(bookmarklet);
 
@@ -25,12 +35,14 @@ if (source) {
     trailingComma: 'none'
   });
 
-  console.log('// decoded and prettier bookmarklet');
+  console.log(success('// decoded and prettier bookmarklet'));
   console.log(formatted);
 
   const clipboardy = require('clipboardy');
   clipboardy.writeSync(formatted);
   clipboardy.readSync();
+} else {
+  console.log(error('Empty input file.'));
 }
 
 process.exitCode = 0;
